769a58c508be63c08f87497814de91ec01382b1e
kaistlayner <layner@kaist.ac.kr>
2023-09-09 01:33:18 +0900

    client and server added (nsIter != ... assertion)

--- app/echo/EchoAssignment.cpp
+++ app/echo/EchoAssignment.cpp
@@ -28,6 +28,61 @@ int EchoAssignment::serverMain(const char *bind_ip, int port,
   // !IMPORTANT: for all system calls, when an error happens, your program must
   // return. e.g., if an read() call return -1, return -1 for serverMain.
 
+  int server_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (server_fd == -1) {
+    perror("socket");
+    return -1;
+  }
+
+  struct sockaddr_in server_addr;
+  memset(&server_addr, 0, sizeof(server_addr));
+
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  server_addr.sin_addr.s_addr = inet_addr(bind_ip);
+
+  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) ==
+      -1) {
+    perror("bind");
+    return -1;
+  }
+
+  if (listen(server_fd, 5) == -1) {
+    perror("listen");
+    return -1;
+  }
+
+  struct sockaddr_in client_addr;
+  memset(&server_addr, 0, sizeof(server_addr));
+  socklen_t client_addr_len;
+
+  int client_fd =
+      accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+  if (client_fd == -1) {
+    perror("accept");
+    return -1;
+  }
+
+  char client_addr_string[INET_ADDRSTRLEN] = {0};
+  inet_ntop(AF_INET, &(client_addr.sin_addr), client_addr_string,
+            INET_ADDRSTRLEN);
+
+  char *buf = new char[20];
+
+  int read_len = read(client_fd, buf, 20);
+
+  if (read_len == -1) {
+    perror("read");
+    return -1;
+  }
+
+  int write_len = write(client_fd, buf, read_len);
+  if (write_len == -1) {
+    perror("write");
+    return -1;
+  }
+
+  submitAnswer(client_addr_string, buf);
   return 0;
 }
 
@@ -38,6 +93,49 @@ int EchoAssignment::clientMain(const char *server_ip, int port,
   // !IMPORTANT: for all system calls, when an error happens, your program must
   // return. e.g., if an read() call return -1, return -1 for clientMain.
 
+  int client_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (client_fd == -1) {
+    perror("socket");
+    return -1;
+  }
+
+  struct sockaddr_in server_addr;
+  memset(&server_addr, 0, sizeof(server_addr));
+
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  server_addr.sin_addr.s_addr = inet_addr(server_ip);
+
+  if (connect(client_fd, (struct sockaddr *)&server_addr,
+              sizeof(struct sockaddr_in)) == -1) {
+    perror("connect");
+    close(client_fd);
+    return -1;
+  }
+
+  char server_addr_string[INET_ADDRSTRLEN] = {0};
+  inet_ntop(AF_INET, &(server_addr.sin_addr), server_addr_string,
+            INET_ADDRSTRLEN);
+
+  char *buf = new char[20];
+
+  int write_len = write(client_fd, command, strlen(command));
+  if (write_len == -1) {
+    perror("write");
+    close(client_fd);
+    return -1;
+  }
+
+  int read_len = read(client_fd, buf, 20);
+  if (read_len == -1) {
+    perror("read");
+    close(client_fd);
+    return -1;
+  }
+
+  submitAnswer(server_addr_string, buf);
+
+  close(client_fd);
   return 0;
 }
 
